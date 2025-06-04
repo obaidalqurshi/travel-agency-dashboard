47c5c7a97484fc8515a07f0f30a3110b64eb6b84
obaidallah <obaidalqershi1@gmail.com>
2025-06-04 19:55:34 +0300

    the payment success page

--- app/routes/root/page-layout.tsx
+++ app/routes/root/page-layout.tsx
@@ -1,26 +1,28 @@
-import { useNavigate } from 'react-router';
-import { logoutUser } from '~/appwrite/auth';
+import {Outlet, redirect, useNavigate} from "react-router";
+import {getExistingUser, logoutUser, storeUserData} from "~/appwrite/auth";
+import {account} from "~/appwrite/client";
+import RootNavbar from "components/RootNavBar";
 
+export async function clientLoader() {
+    try {
+        const user = await account.get();
 
+        if(!user.$id) return redirect('/sign-in');
+
+        const existingUser = await getExistingUser(user.$id);
+        return existingUser?.$id ? existingUser : await storeUserData();
+    } catch (e) {
+        console.log('Error fetching user', e)
+        return redirect('/sign-in')
+    }
+}
 
 const PageLayout = () => {
-    
-const navigate = useNavigate();
-const handleLogout = async () => {
-    await logoutUser();
-    navigate('/sign-in')
-}
     return (
-        <div>
-            <button onClick={handleLogout} className="cursor-pointer">
-            <img src="/assets/icons/logout.svg" alt="logout" className="size-6" referrerPolicy="no-referrer" />
-            
-            </button>
-            <button onClick={()=> navigate('/dashboard')}>
-            Dashbboard
-            </button>
-            </div>
+        <div className="bg-light-200">
+            <RootNavbar />
+            <Outlet />
+        </div>
     )
 }
-
 export default PageLayout
